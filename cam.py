42d5507724fad289ed7f9d2a05b58fc61d07a68b
hunar3020 <107911462+hunar3020@users.noreply.github.com>
2022-06-21 12:17:42 +0530

    Create cam.py

---
+++ cam.py
@@ -0,0 +1,30 @@
+from picamera import PiCamera
+from time import sleep
+import subprocess
+import datetime
+
+
+dt = datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S')
+dateStr,timeStr = dt.split()
+
+def captureImage():
+
+  camera = PiCamera()
+  camera.start_preview()
+  print("Capturing image")
+  sleep(5)
+  camera.capture('/home/pi/Desktop/image.jpg')
+  print("Image capture done")
+  camera.stop_preview()
+
+captureImage()
+
+subprocess.run(["scp","/home/pi/Desktop/image.jpg","user@192.168.43.108:/home/user"])
+
+
+
+
+ImageProc.py
+import cv2
+
+
